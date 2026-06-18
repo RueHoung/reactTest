f141de10368f622834d3486e1a09481c93ff4f99
Jeff Tsao <jeff_tsao@megaforce.com.tw>
2026-06-18 16:43:34 +0800

    [NEW]Firebase google auth

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
@@ -14,4 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app;
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
--- src/view/Login/Login.tsx
+++ src/view/Login/Login.tsx
@@ -1,63 +1,52 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth"
+import type { User } from "firebase/auth";
+import { useState, useEffect } from "react"
+import { auth, googleProvider } from "@/services/firebase";
 
 export function Login() {
+    
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser: User | null) => {
+        setUser(currentUser);
+        });
+        return () => unsubscribe();
+    }, []);
+    const loginWithGoogle = async () => {
+        try {
+        await signInWithPopup(auth, googleProvider);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error(error.message);
+            } else {
+                console.error(error);
+            }
+        }
+    };
+
+    const logout = async () => {
+        await signOut(auth);
+    };
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
-      </CardFooter>
-    </Card>
+    <div>
+      {user ? (
+        <>
+            <h2>Hi, {user.displayName}</h2>
+            <p>{user.email}</p>
+            {user.photoURL && (
+                <img
+                    src={user.photoURL}
+                    alt="avatar"
+                    width={80}
+                />
+            )}
+            <br />
+            <button onClick={logout}>登出</button>
+        </>
+        ) : (
+        <button onClick={loginWithGoogle}>使用 Google 登入</button>
+      )}
+    </div>
   )
 }
